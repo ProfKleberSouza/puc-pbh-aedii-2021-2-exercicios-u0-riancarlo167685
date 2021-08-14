e12a32f196c9c1508ab78878035ed42b3edcf9e5
Rian Oliveira <88520011+riancarlo167685@users.noreply.github.com>
2021-08-14 01:35:21 +0000

    Exercício 5

--- exe05/main.c
+++ exe05/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int n1, n2, n3, n4;
+   float media;
+
+   scanf("%i %i %i %i", &n1, &n2, &n3, &n4);
+
+   media = (float)(n1+n2+n3+n4)/4;
+
+   if(media>=6)
+    printf("NOTA = %.1f (APROVADO)", media);
+   else
+    printf("NOTA = %.1f (REPROVADO)", media);
+   
    return 0;
 }
