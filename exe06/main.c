e6c6c4fb1a07cad7d457e1bb94cdd0dba2800611
Rian Oliveira <88520011+riancarlo167685@users.noreply.github.com>
2021-08-28 22:51:42 +0000

    Exercício 6

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+  int X, Y, Z;
+  scanf("%i %i %i", &X, &Y, &Z);
+  
+  if((abs(Y-Z)<X && X<(Y + Z))&&(abs(X-Z)<Y && Y<(X + Z))&&(abs(X-Y)<Z && Z<(X + Y))){
+    if(X==Y && Y==Z)
+      printf("TRIANGULO EQUILÁTERO");
+    else if(X!=Y && Y!=Z && X!=Z)
+      printf("TRIANGULO ESCALENO");
+    else if(X==Y || Y==Z || X==Z)
+      printf("TRIANGULO ISOSCELES");
+    }else
+      printf("OS LADOS NAO FORMAM UM TRIANGULO");
+
    return 0;
 }
