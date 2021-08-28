2a494874da05a66c1aedf9a29354117c941aabeb
Rian Oliveira <88520011+riancarlo167685@users.noreply.github.com>
2021-08-28 23:14:52 +0000

    Exercício 7

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+  int n,x;
+  int menor, maior=0;
+  
+  scanf("%i", &n);
+  
+  for(int i=0;i<n;i++){
+    scanf("%i", &x);
+    if(i==0)
+      menor = x;
+    if(x<menor)
+      menor = x;
+    else if(x>maior)
+      maior = x;
+  }
+  printf("MENOR = %i\n", menor);
+  printf("MAIOR = %i\n", maior);
+
    return 0;
 }
